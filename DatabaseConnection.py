8cd575e80a7fbe149944a2bcce9e7bcbf82ea320
marcosvgj <marcos.guimaraesnetwork@gmail.com>
2018-01-12 23:25:16 -0200

    Adaptação da classe MongoDB aos padrões PEP-8

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -1,38 +1,39 @@
 #!/usr/bin/python
 
-from pymongo import MongoClient
-import datetime
+from pymongo import errors, MongoClient
 
-#Intenção: Desacoplar o acesso ao banco de dados da classe Coletor
+# Intenção: Desacoplar o acesso ao banco de dados da classe Coletor
 
 class MongoDB:
-    def __init__(self,host,port):
+    def __init__(self, host=None, port=None):
+
+        self.session = MongoClient()
 
         # Default Host
-        if(host is None): self.host = 'localhost'
-        else: self.host = host
+        if host is None:
+            self.host = 'localhost'
+        else:
+            self.host = host
 
         # Default Port
-        if(port is None): self.port = 27017
-        else: self.port = port
+        if port is None:
+            self.port = 27017
+        else:
+            self.port = port
 
+    def iniciar_sessao(self):
+        self.session = MongoClient(self.host, self.port)
 
+    def fechar_sessao(self):
+        self.session.close()
 
-    def iniciarSessao(self):
-        self.Session = MongoClient(self.host, self.port)
+    def armazenar_dados(self, coletor):
 
-    def fecharSessao(self):
-        self.Session.close()
+        db = self.session['SmartCoinDB'].get_collection(coletor.RequestType.market)
 
-    def armazenarDados(self,Coletor):
-        db = self.Session['SmartCoinDB'].get_collection(Coletor.RequestType.market)
-        db.insert_one(self.data)
+        try:
+            db.insert_one(coletor.data)
+            print("Data was successfully inserted in the follow Colletion: %s " % coletor.RequestType.market)
 
-
-
-
-
-
-
-
-
+        except errors.OperationFailure as error:
+            print("Could not apply the operation %s" % error)
