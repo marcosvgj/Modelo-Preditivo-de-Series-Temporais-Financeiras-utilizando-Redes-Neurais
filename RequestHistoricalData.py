cd39f39560bcff735cfe54c50c9fae09c4a70977
marcosvgj <marcos.guimaraesnetwork@gmail.com>
2018-02-21 04:09:11 -0300

    Adição da funcionalidade Coletor Histórico

---
+++ RequestHistoricalData.py
@@ -0,0 +1,69 @@
+import abc
+import json
+from datetime import date, timedelta
+from threading import Thread
+import requests
+
+
+class RequestHistorico(Thread):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def request_data(self):
+        """Assinatura da função que realiza a requisição de dados"""
+
+
+class MercadoBitcoin(RequestHistorico):
+    market = "Mercado Bit Coin"
+
+    def __init__(self, coin, date_request):
+        super().__init__()
+
+        self.coin = coin
+        self.date_request = date_request
+        self.source = "https://www.mercadobitcoin.net/api/" + self.coin + "/day-summary/" + self.date_request
+        self.data = {}
+
+    def request_data(self):
+
+        try:
+            requisition = requests.get(self.source)
+            self.data.update(json.loads(requisition.text))
+            self.data.update({"coin": self.coin})
+
+        except requests.exceptions.ConnectionError as error:
+
+            print("<Erro na requisição dos dados> : %s" % error)
+
+        except requests.exceptions.Timeout as error:
+
+            print("<Erro na requisição dos dados> : %s " % error)
+
+
+class ColetorHistorico:
+    cryptocurrencies = ['BTC', 'LTC', 'BCH']
+
+    def __init__(self, request_type):
+        self.data = {}
+        self.utc_offset = 2
+        self.request_type = request_type
+
+    def get_historico(self, dinicio=None):
+        if dinicio is None:
+            dinicio = date(2014, 1, 1)
+
+        dfim = date.today() - timedelta(days=1)
+        periodo = dfim - dinicio
+
+        for i in range(periodo.days + 1):
+            date_parsing = str(dinicio + timedelta(days=i))
+            date_request = date_parsing.replace("-", '/')
+
+            iterator = [self.request_type(item, date_request) for item in self.cryptocurrencies]
+            for item in iterator:
+                item.request_data()
+                print(item.data)
+
+
+coletor = ColetorHistorico(MercadoBitcoin)
+coletor.get_historico()
